36c7b8e8e589f9e7f5d2b1a15e023ad2319cfdab
nhhor <nhhor@post.com>
2020-04-07 14:00:09 -0700

    update Eula fade text.

--- src/components/EulaText.js
+++ src/components/EulaText.js
@@ -19,50 +19,23 @@ function EulaText(props){
       </p>
       <p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures. Completely iterate covalent strategic theme areas via accurate e-markets.
       </p>
-      <div className='test'>
-      </div>
+
+      <hr/>
+
       <style>
-        {`
+        {
+          `
           .EulaText {
             text-align: left;
-            height: 50vh;
+            height: 66%;
             overflow: auto;
             padding: 25px;
-
-            position:relative;
-            // color:#fff;
-            // overflow:hidden;
-
           }
+          `
+        }
+      </style>
+    </div>
+  );
+}
 
-          // .EulaTextBackground {
-          //   background: rgb(255,255,255);
-          //   background: linear-gradient(180deg, rgba(255,255,255,0.01) 0%, rgba(255,255,255,0.99) 25%, rgba(255,255,255,1) 100%);
-          // }
-
-          // 
-          // .test {
-          //   background-repeat: no-repeat;
-          //   background-attachment: fixed;
-          //   position: fixed;
-          //   bottom:24vh;
-          //   left: 33.3%;
-          //   width:33.3%;
-          //   height: 50%;
-          //   // background: -moz-linear-gradient(top,  rgba(137,255,241,0) 0%, rgba(0,0,0,1) 100%); /* FF3.6+ */
-          //   // background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(137,255,241,0)), color-stop(100%,rgba(0,0,0,1))); /* Chrome,Safari4+ */
-          //   // background: -webkit-linear-gradient(top,  rgba(137,255,241,0) 0%,rgba(0,0,0,1) 100%); /* Chrome10+,Safari5.1+ */
-          //   // background: -o-linear-gradient(top,  rgba(137,255,241,0) 0%,rgba(0,0,0,1) 100%); /* Opera 11.10+ */
-          //   // background: -ms-linear-gradient(top,  rgba(137,255,241,0) 0%,rgba(0,0,0,1) 100%); /* IE10+ */
-          //   background: linear-gradient(180deg, rgba(255,255,255,0.01) 0%, rgba(255,255,255,0.99) 99%, rgba(255,255,255,1) 100%);
-          //   // filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0089fff1', endColorstr='#000000',GradientType=0 ); /* IE6-9 */
-          //
-          // }
-
-            `}
-          </style>
-        </div>
-      );
-    }
-
-    export default EulaText;
+export default EulaText;
