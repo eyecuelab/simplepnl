3865725a2c15a9105c1aa61228635fb8127d9b5f
Celeste Morton <creneemorton@gmail.com>
2020-04-10 13:20:33 -0700

    add successful oauth integration to home screen

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,21 +1,6 @@
+/* global gapi */
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
-// function NotSignedIn() {
-//     return (
-//       <div>
-//         <p>You are not signed in. Click here to sign in.</p>
-//         <button id="loginButton">Login with Google</button>
-//       </div>
-//     )
-//   }
-  
-//   function Members() {
-//     return (
-//       <p>hello user, you're signed in </p>
-//     )
-//   }
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
@@ -45,7 +30,7 @@ class OAuth extends Component {
         var opts = {
           width: 200,
           height: 50,
-          onSuccess: this.successCallback
+          onSuccess: successCallback
         }
         gapi.signin2.render('loginButton', opts)
       })
@@ -74,7 +59,6 @@ class OAuth extends Component {
       render() {
         return (      
           <div>
-              {/* <Route exact path="/" render={this.authCheck.bind(this, Home)}/> */}
               {this.getContent()}           
           </div>
         );
