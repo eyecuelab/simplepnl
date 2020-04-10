9d2248043fc154bd0bcecf39059f05d82c7f903c
nhhor <nhhor@post.com>
2020-04-10 10:42:39 -0700

    edit ActionTypes, ad log to index.js, edit oauth-reducer.

--- src/index.js
+++ src/index.js
@@ -28,6 +28,8 @@ ReactDOM.render(
 
 unsubscribe();
 
+console.log('store.getState(): ', store.getState());
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
