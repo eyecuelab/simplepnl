50506c8f87d09c7a9a7e10265126dcc68c0b2c51
nhhor <nhhor@post.com>
2020-04-15 15:04:32 -0700

    update DisplayHeader with unique'ish key generation.

--- src/components/DisplayHeader.js
+++ src/components/DisplayHeader.js
@@ -3,64 +3,75 @@ import { connect } from 'react-redux';
 
 
 class DisplayHeader extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            // reportName: null,
-            // headerRow: null,
-            // dateColumn: null,
-            // descriptionColumn: null,
-            // twoAmountCoulumns: false,
-            // amountColumn: null,
-            // creditColumn: null,
-            // debitColumn: null,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      // reportName: null,
+      // headerRow: null,
+      // dateColumn: null,
+      // descriptionColumn: null,
+      // twoAmountCoulumns: false,
+      // amountColumn: null,
+      // creditColumn: null,
+      // debitColumn: null,
+    };
+    this.newKey = 0;
+  }
 
-    csvReturn() {
-        const {csv} = this.props
-        return(
-            <table style={table}>
-                <tbody>
-                     {
-                      csv.slice(0, 3).map((row, i) =>(
-                               <tr key={i}>
-                                   {
-                                       row.data.map((column,j) =>
-                                   <td style={rows} key={j}>{column}</td>
-                                       )
-                                
-                                   }
-                               </tr>
-                            ))
-                         }
-                    </tbody>
-                </table>
-        )
+  csvReturn() {
+    const { csv } = this.props;
+    if (!csv) {
+      return (
+        <div>
+          <h5>
+            Whoops forgot to upload a CSV...<br /> We better add a check for that!
+          </h5>
+          <hr />
+        </div>
+      );
     }
+    return (
+      <table style={table}>
+        <tbody>
+          {
+              csv.slice(0, 3).map((row, i) => (
+                <tr id={`row_${i}`} key={this.newKey++}>
+                  {
+                    row.data.map((column, j) => (
+                      <td id={`row_${i}-column_${j}`} style={rows} key={this.newKey++}>{column}</td>
+                    ))
+
+                  }
+                </tr>
+              ))
+            }
+        </tbody>
+      </table>
+    );
+  }
 
 
-    render() {
-        return (
-            <div>
-                {this.csvReturn()}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.csvReturn()}
+      </div>
+    );
+  }
 }
 
 const table = {
-    marginBottom: '50px'
-}
+  marginBottom: '50px',
+};
 
 const rows = {
-    borderBottom: '1px solid rgba(0,0,0,.1)',
-    padding: '5px',
-    fontSize: '15px'
-}
+  borderBottom: '1px solid rgba(0,0,0,.1)',
+  padding: '5px',
+  fontSize: '15px',
+};
 
 const mapStateToProps = (state) => ({
-    csv: state.csvReducer.payload
+  csv: state.csvReducer.payload,
 });
 
-export default connect(mapStateToProps)(DisplayHeader)
+export default connect(mapStateToProps)(DisplayHeader);
