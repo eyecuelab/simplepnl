e26b8b96b9071b0256505cffceac0c665dfbb263
Celeste Morton <creneemorton@gmail.com>
2020-04-20 17:22:51 -0700

    add editpnl for updating existing pnl reports

--- src/components/EditPnl.js
+++ src/components/EditPnl.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
 import { ConfirmButton } from '../styles/components';
 
 class EditPnl extends React.Component {
   updateSheetValues = () => {
-    fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`, {
+    fetch(`https://sheets.googleapis.com/v4/spreadsheets?alt=json`, {
       method: "POST",
       headers: {
         "Content-Types": "application/json",
-        //update this token with yours. 
         // Authorization: `Bearer ${ACCESS_TOKEN}`,
            },
            body: JSON.stringify({
