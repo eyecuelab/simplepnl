6f04d82d286c28c2a97242e96b8cf82c8eaa3a5e
Celeste Morton <creneemorton@gmail.com>
2020-04-22 13:27:59 -0700

    add updates to the select files for rows vs columns

--- src/components/SelectDescription.js
+++ src/components/SelectDescription.js
@@ -12,8 +12,7 @@ function SelectDescription() {
       <PinkLine />
       <div className="SelectDescription">
         <SelectPreview>Here&apos;s a preview of your CSV.</SelectPreview>
-        <h6>I think I found the <span className="extraBold">description</span> row <span className="highlightedCell">below</span>.</h6>
-        <h6>Can you double check by tapping on the <span className="extraBold">description</span> row and confirming?</h6>
+        <h6>Please click on the <span className="extraBold">DESCRIPTION</span> column below and hit confirm!</h6>
 
         <hr />
         <DescriptionColumn />
