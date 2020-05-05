b9f7c3bb88d2e20748ff38d56bceadcd54a4a26e
nhhor <nhhor@post.com>
2020-05-04 17:30:35 -0700

    add Google verification file, Privacy components.

---
+++ src/components/Privacy.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MegHeader from './MegHeader';
+import PrivacyText from './PrivacyText';
+import { EulaContainer, EulaHeader, EulaButton, EulaButtonContainer, EulaFadeContainer } from '../styles/components';
+
+
+function Privacy() {
+  return (
+    <EulaContainer>
+      <MegHeader />
+      <EulaHeader>Privacy Policy for Eyecue Lab and SimplePnL</EulaHeader>
+      <PrivacyText />
+      <EulaButtonContainer>
+        <Link to="/">
+          <EulaButton>
+            Right on.
+          </EulaButton>
+        </Link>
+      </EulaButtonContainer>
+      <EulaFadeContainer />
+
+      <style>
+        {
+          `
+
+          .EulaButtonReady {
+            font-size: 1rem;
+            font-weight: 500;
+          }
+
+          .EulaButtonGo {
+            font-size: 1rem;
+            font-weight: 800;
+          }
+
+          `
+        }
+      </style>
+    </EulaContainer>
+  );
+}
+
+export default Privacy;
